5fd8ac01be8ae9d9dacb603fe79b443d2409a328
Tao Hu <taohu620@gmail.com>
2017-12-15 00:32:55 +0800

    u

--- mxnetgo/myutils/precheck.py
+++ mxnetgo/myutils/precheck.py
@@ -1,13 +1,4 @@
 # Author: Tao Hu <taohu620@gmail.com>
 import logger
-def print_params(arg_params, aux_params):
-    logger.info("arg_params: ")
-    logger.info("{:<30}  {:<20}".format("name","shape"))
-    for k,v in arg_params.items():
-        logger.info("{:<30}  {:<20}".format(k, v.shape))
 
-    logger.info("aux_params: ")
-    logger.info("{:<30}  {:<20}".format("name", "shape"))
-    for k,v in aux_params.items():
-        logger.info("{:<30}  {:<20}".format(k, v.shape))
 
